c65dcb2d5445c8bf2e11f477c0a94fdb4ab7628c
Muhammad Muneeb <muneebn048@gmail.com>
2026-03-26 19:37:18 +0500

    check

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -27,7 +27,5 @@ router.post("/logout", protect, logout);
 router.put("/profile", protect, updateProfile);
 router.put("/password", protect, updatePassword);
 
-// PUT /api/auth/admin/:id/suspend
-router.put("/admin/:id/suspend", protect, authorize("super_admin"), toggleAdminSuspension);
 
 module.exports = router;
